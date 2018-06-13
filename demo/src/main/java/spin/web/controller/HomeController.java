8c3f04c471f0c5763e4721041da84272d630f9f7
Spin <spinshanxi@gmail.com>
2018-06-13 17:21:58 +0800

    modify owner

--- demo/src/main/java/spin/web/controller/HomeController.java
+++ demo/src/main/java/spin/web/controller/HomeController.java
@@ -55,7 +55,7 @@ public class HomeController {
         try {
             con = dataSource.getConnection();
             System.out.println(con.getSchema());
-            ps = con.prepareStatement("select id, name, review from phone");
+            ps = con.prepareStatement("select id, name, review from public.phone");
             rs = ps.executeQuery();
             System.out.println(rs.getMetaData());
             while (rs.next()) {
